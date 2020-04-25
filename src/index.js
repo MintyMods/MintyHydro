84e2413e6b758dcc95421b57842db51e484e52e5
Rob Gregory <2349765+MintyMods@users.noreply.github.com>
2020-04-25 18:03:03 +0100

    Upgrade font awesome icons to v5.13.0 Pro

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import '../style/style.css';
 import io from 'socket.io-client';
 import { url } from '../mintyConfig';
 
+
 (function() {
     // Connect to the socket server
     const socket = io.connect(url);
